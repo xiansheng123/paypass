fdfb53aec09a42ee45adce6a2208625f4fb846b8
xiansheng123 <732824600@qq.com>
2021-08-26 11:43:38 +0800

    init project

---
+++ src/main/java/com/example/paypass/PaypassApplication.java
@@ -0,0 +1,13 @@
+package com.example.paypass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaypassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaypassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/paypass/PaypassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paypass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaypassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
